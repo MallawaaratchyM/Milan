d6100cf88d02c08c8fb081cae7d3a52ead2371cf
MallawaaratchyM <55143271+MallawaaratchyM@users.noreply.github.com>
2019-09-17 08:06:12 -0500

    Add files via upload

---
+++ APCS2019/src/inputoutput/asciibox.java
@@ -0,0 +1,29 @@
+package inputoutput;
+
+public class asciibox  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+
+	}
+
+}
