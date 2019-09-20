6cdbb250a5712697c08d67faa306ca5b43b7ab52
784187 <784187@10.9.30.126>
2019-09-20 08:19:56 -0500

    Im about to start birthday game

---
+++ APCS2019/src/inputoutput/asciiart.java
@@ -0,0 +1,54 @@
+package inputoutput;
+
+public class asciiart {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\_\\_    _/_/");
+		System.out.println("    \\__/");
+		System.out.println("    (oo)\\__________________________________");
+		System.out.println("    (-_)\\                                  )\\/\\");	
+		System.out.println("        ||--------------------------------||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("        ||                                ||");
+		System.out.println("       /  \\                              /  \\");
+		System.out.println("       \\__/                              \\__/");
+
+
+}
+}
+
---
+++ APCS2019/src/inputoutput/nameTesting.java
@@ -0,0 +1,23 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+public class nameTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		var keyboard = new Scanner(System.in);
+		String names="-";
+		int age=0;
+		System.out.println("Hey there buddy, what's your name?");
+		names=keyboard.nextLine();
+		
+		System.out.println("Hey "+names+", cuantos años tienes?");
+		age=keyboard.nextInt();
+		age=100-age;
+		System.out.println(names+", you will be 100 in "+age+" years. If you survive... >:) XD");
+		
+	}
+
+}
