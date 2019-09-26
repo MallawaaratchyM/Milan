90202bf759c3f39d9515d678d45ab17b29066916
784187 <784187@10.9.30.126>
2019-09-26 08:18:21 -0500

    fini

---
+++ APCS2019/src/inputoutput/BirthGamer.java
@@ -0,0 +1,24 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+public class BirthGamer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Determine your birth month (January=1, February=2, etcetera).");
+		System.out.println("Multiply the number by 5");
+		System.out.println("Add 6");
+		System.out.println("Multiply it by 4");
+		System.out.println("Add 9");
+		System.out.println("Multiply it by 5");
+		System.out.println("Add your birthday to the number (8 if you were born on the 8th)");
+		System.out.println("Enter your number");
+		var keyboard = new Scanner(System.in);
+		int age=keyboard.nextInt();
+		age=age-165;
+		int jury5=0;
+		
+	}
+
+}
