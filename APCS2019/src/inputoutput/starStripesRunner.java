0d3c60bb7ecff6d999ee47b00a6c454b5dbd1f0a
MallawaaratchyM <55143271+MallawaaratchyM@users.noreply.github.com>
2019-10-04 07:58:07 -0500

    Add files via upload
    These are the stars and stripes labss

---
+++ APCS2019/src/inputoutput/starStripesRunner.java
@@ -0,0 +1,38 @@
+package inputoutput;
+
+public class starStripesRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StarStripes star = new StarStripes();
+		
+		
+		star.printTwentyDashes();
+		star.printTwentyStars();
+		star.printTwentyDashes();
+		star.printTwentyStars();
+		star.printTwentyDashes();
+		star.printTwentyStars();
+		star.printTwentyDashes();
+		star.printTwoBlankLines();
+		star.printTwentyDashes();
+		
+		star.printTwentyDashes();
+		star.printTwentyStars();
+		star.printTwentyDashes();
+		star.printTwentyStars();
+		star.printTwentyDashes();
+		star.printTwentyStars();
+		star.printTwentyDashes();
+		star.printTwentyDashes();
+		star.printTwentyStars();
+		star.printTwentyDashes();
+		star.printTwentyStars();
+		star.printTwentyDashes();
+		star.printTwentyStars();
+		star.printTwentyDashes();
+		
+	}
+
+}
