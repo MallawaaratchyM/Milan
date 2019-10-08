3fb5fa2ef800b570c4ecc389bf247e9dcc1d1205
784187 <784187@10.9.30.126>
2019-10-08 07:58:07 -0500

    Done with mister smileuy

--- APCS2019/src/methods/GraphicsRunner.java
+++ APCS2019/src/methods/GraphicsRunner.java
@@ -37,4 +37,5 @@ public class GraphicsRunner extends JFrame
 	{
 		GraphicsRunner run = new GraphicsRunner();
 	}
+	
 }
--- APCS2019/src/methods/SmileyFace.java
+++ APCS2019/src/methods/SmileyFace.java
@@ -10,13 +10,14 @@ public class SmileyFace extends Canvas
    public SmileyFace()    //constructor - sets up the class
    {
       setSize(800,600);
-      setBackground(Color.WHITE);
+      setBackground(Color.CYAN);
       setVisible(true);
    }
 
    public void paint( Graphics window )
    {
       smileyFace(window);
+      
    }
 
    public void smileyFace( Graphics window )
@@ -24,9 +25,18 @@ public class SmileyFace extends Canvas
       window.setColor(Color.BLUE);
       window.drawString("SMILEY FACE LAB ", 35, 35);
 
-      window.setColor(Color.YELLOW);
+      window.setColor(Color.BLACK);
       window.fillOval( 210, 100, 400, 400 );
-
+      
+      window.setColor(Color.RED);
+      window.fillOval(300, 159, 69, 69);
+      
+      window.setColor(Color.RED);
+      window.fillOval(439, 159, 69, 69);
+      
+      window.setColor(Color.WHITE);
+      window.fillArc(260, 220, 291, 180, 180, 180);
+     
 		//add more code here
 
 
