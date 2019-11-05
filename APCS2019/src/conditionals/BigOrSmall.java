a3f357089ae6052543203d3ae4295d6d19d129d4
784187 <784187@fs-a-rhsk22.district196.org>
2019-11-05 07:57:01 -0600

    good job joe

---
+++ APCS2019/src/conditionals/BigOrSmall.java
@@ -0,0 +1,30 @@
+package conditionals;
+import static java.lang.System.*; 
+import java.util.Scanner;
+public class BigOrSmall {
+
+	//(c) A+ Computer Science
+	//www.apluscompsci.com
+	//Name -
+
+	
+
+		public static String check( int a, int b )
+		{
+			
+			if(a>b)
+			{
+			return "yes";
+			}
+			
+			if(a<b)
+			{
+			return "no";
+			}
+			else
+			{
+				return "aplus";
+			}
+		}
+}
+
