a4f381d49395ef45d07b702261f32e2e03ad394c
784187 <784187@fs-a-rhsk22.district196.org>
2019-11-13 08:00:57 -0600

    Good night

---
+++ APCS2019/src/conditionals/NumberVerify.java
@@ -0,0 +1,20 @@
+package conditionals;
+import static java.lang.System.*;
+public class NumberVerify {
+
+	public static boolean isOdd( int num )
+	{
+		if(num%2!=0)
+		return true;
+		return false;
+		
+	}
+	public static boolean isEven( int num )
+	{
+		if(num%2==0)
+		return true;
+		return false;
+			
+	
+	}
+}
